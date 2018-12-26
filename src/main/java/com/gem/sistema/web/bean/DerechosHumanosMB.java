f593e0de813146fcc257af8ca0b578ce02ae15cd
=Alfredo Neri <=>
2018-12-26 13:38:33 -0600

    Fixes: FOLIO: 00025
    Se crean componentes para el reporte de Competencia Laboral del Defensor
    Municipal de los Derechos Humanos

---
+++ src/main/java/com/gem/sistema/web/bean/DerechosHumanosMB.java
@@ -0,0 +1,136 @@
+package com.gem.sistema.web.bean;
+
+import static com.roonin.utils.UtilDate.getLastDay;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+import org.primefaces.model.StreamedContent;
+
+import com.gem.sistema.business.domain.Firmas;
+import com.gem.sistema.business.domain.TcPeriodo;
+import com.gem.sistema.business.repository.catalogs.FirmasRepository;
+import com.gem.sistema.business.repository.catalogs.TcMesRepository;
+import com.gem.sistema.business.repository.catalogs.TcPeriodoRepositoy;
+import com.gem.sistema.business.service.reportador.ReportValidationException;
+
+@ManagedBean(name = "derechosHumanosMB")
+@ViewScoped
+public class DerechosHumanosMB extends BaseDirectReport {
+
+	private List<TcPeriodo> listSemestres;
+	private Integer semestre;
+	private Firmas firmas;
+
+	@ManagedProperty("#{tcPeriodoRepositoy}")
+	private TcPeriodoRepositoy tcPeriodoRepositoy;
+
+	@ManagedProperty("#{firmasRepository}")
+	private FirmasRepository firmasRepository;
+
+	@ManagedProperty("#{tcMesRepository}")
+	private TcMesRepository tcMesRepository;
+
+	@PostConstruct
+	public void init() {
+		listSemestres = tcPeriodoRepositoy.findByTipoPeriodo(6);
+		jasperReporteName = "defensorDerechosHumanos";
+		endFilename = jasperReporteName + ".pdf";
+
+		if (!listSemestres.isEmpty()) {
+			semestre = listSemestres.get(0).getPeriodo();
+		}
+
+	}
+
+	@Override
+	public Map<String, Object> getParametersReports() throws ReportValidationException {
+		Map<String, Object> parameters = new java.util.HashMap<String, Object>();
+
+		firmas = firmasRepository.findAllByIdsector(this.getUserDetails().getIdSector());
+		Object[] meses = getMonthsBySemestre(semestre);
+		
+		parameters.put("municipioName", firmas.getCampo1());
+		parameters.put("imagen", this.getUserDetails().getPathImgCab1());
+		parameters.put("anio", firmas.getCampo3());
+		parameters.put("mes", meses[1]);
+		parameters.put("dia", meses[2]);
+		parameters.put("firmaL1", firmas.getL32());
+		parameters.put("firmaN1", firmas.getN32());
+		
+		return parameters;
+	}
+
+	@Override
+	public StreamedContent generaReporteSimple(int type) throws ReportValidationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object[] getMonthsBySemestre(Integer semestre) {
+		Integer mesFinal = semestre * 6;
+		Integer mesInicial = mesFinal - 5;
+		Object[] meses = {
+				tcMesRepository.findByMes(org.apache.commons.lang3.StringUtils.leftPad(mesInicial.toString(), 2, "0"))
+						.getDescripcion(),
+				tcMesRepository.findByMes(org.apache.commons.lang3.StringUtils.leftPad(mesFinal.toString(), 2, "0"))
+						.getDescripcion(),
+				getLastDay(mesFinal) };
+
+		return meses;
+	}
+	
+	public Integer getSemestre() {
+		return semestre;
+	}
+
+	public void setSemestre(Integer semestre) {
+		this.semestre = semestre;
+	}
+
+	public List<TcPeriodo> getListSemestres() {
+		return listSemestres;
+	}
+
+	public void setListSemestres(List<TcPeriodo> listSemestres) {
+		this.listSemestres = listSemestres;
+	}
+
+	public Firmas getFirmas() {
+		return firmas;
+	}
+
+	public void setFirmas(Firmas firmas) {
+		this.firmas = firmas;
+	}
+
+	public TcPeriodoRepositoy getTcPeriodoRepositoy() {
+		return tcPeriodoRepositoy;
+	}
+
+	public void setTcPeriodoRepositoy(TcPeriodoRepositoy tcPeriodoRepositoy) {
+		this.tcPeriodoRepositoy = tcPeriodoRepositoy;
+	}
+
+	public FirmasRepository getFirmasRepository() {
+		return firmasRepository;
+	}
+
+	public void setFirmasRepository(FirmasRepository firmasRepository) {
+		this.firmasRepository = firmasRepository;
+	}
+
+	public TcMesRepository getTcMesRepository() {
+		return tcMesRepository;
+	}
+
+	public void setTcMesRepository(TcMesRepository tcMesRepository) {
+		this.tcMesRepository = tcMesRepository;
+	}
+
+}
