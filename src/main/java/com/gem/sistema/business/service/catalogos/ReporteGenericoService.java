4a8e1c3134cdab79f0c434e3400e923e38308ae6
=Alfredo Neri <=>
2019-01-03 09:23:04 -0600

    Fixes FOLIO:00015
    Se generán componentes para el reporte Programa Anual de Obra
    (Reparación y Mantenimiento)

---
+++ src/main/java/com/gem/sistema/business/service/catalogos/ReporteGenericoService.java
@@ -0,0 +1,6 @@
+package com.gem.sistema.business.service.catalogos;
+
+public interface ReporteGenericoService {
+
+	public String getFileTxtWithIdSector(Integer idReporte, Integer idSector, String nameFile);
+}
